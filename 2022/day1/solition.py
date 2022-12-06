a6d60c4fd27b72cb7fbde15949531f1c6ac860c9
ruben.galvan <ruben.g1293@gmail.com>
2022-12-05 23:16:02 -0600

    day 1 part 2, get top 3

--- 2022/day1/solition.py
+++ 2022/day1/solition.py
@@ -1,28 +1,20 @@
 def day1_solution():
     count = 0
-    elfs = {}
+    elfs = []
 
     with open('input.txt', 'r') as f:
         calories = []
-        big_in_calories = None
 
         for line in f:
             if line.isspace():
                 count += 1
-                elfs[count] = sum(calories)
-
-                if len(elfs) == 1:
-                    big_in_calories_elf = count
-                else:
-                    if elfs[count] > elfs[big_in_calories_elf]:
-                        big_in_calories_elf = count
-
+                elfs.append(sum(calories))
                 calories = []
             else:
                 calories.append(int(line))
 
-        print('Big in calories elf', big_in_calories_elf, 'with', elfs[big_in_calories_elf])
-
+        elfs.sort(reverse=True)
+        print(sum(elfs[0:3]))
 
 
 if __name__ == '__main__':
