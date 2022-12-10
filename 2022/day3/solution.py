a4c576e0469e03184cdd63ee79580a71b56f2efe
ruben.galvan <ruben.g1293@gmail.com>
2022-12-10 15:21:51 -0600

    part two day3

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -16,15 +16,27 @@ def get_type_value(type):
 
 def day3_solution():
     rucksacks = []
+    elves_groups = []
 
     current_dir = dirname(__file__)
     file_path = join(current_dir, "./input.txt")
 
     with open(file_path, 'r') as f:
+        count = 0
+        group = []
+
         for line in f:
+            count += 1
             items = line.rstrip()
             lenght_items = len(items)
             list_items = list(items)
+            group.append(set(list_items))
+
+            if count == 3:
+                elves_groups.append(group)
+                count = 0
+                group = []
+
             half_len = int(lenght_items / 2)
             first_half = set(list_items[0:half_len])
             second_half = set(list_items[half_len:lenght_items])
@@ -33,10 +45,17 @@ def day3_solution():
             same_type = first_half & second_half
             type = same_type.pop()
             same_type_value = get_type_value(type)
-            print('Compartiments for:', line, type, same_type_value, lenght_items)
             rucksacks.append(same_type_value)
+            # print('Elves groups:', elves_groups)
         
-        print(sum(rucksacks))
+        groups_priority = []
+        for group in elves_groups:
+            badge = set.intersection(*group)
+            badge_type = badge.pop()
+            groups_priority.append(get_type_value(badge_type))
+            # print('Badge type', )
+        print('Groups priority', sum(groups_priority))
+        print('Rucksacks', sum(rucksacks))
 
 
 
